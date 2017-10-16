dd091a79fa8f55fd7e6ff9af207193c8b8ae9020
cattlelin <cattlelin@springwoods.com>
2017-10-16 20:43:46 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app-box">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+@import './assets/less/reset.less'; 
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      登录页面！
+  </div>
+</template>
+<script>
+    export default {
+        data (){
+            return  {
+
+            }
+        }
+    }
+</script>
+<style lang="less">
+    body{
+        background: #ddd;
+    }
+    div{
+        font-size: 28px;
+        font-weight: 800;
+        text-align: center;
+        margin-top: 20px;
+    }
+</style>
